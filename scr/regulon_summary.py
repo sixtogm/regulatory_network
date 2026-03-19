3f5d0fb1422cccd6ab5aa47ec70653d6ad7d338e
Sixto Netzahualcóyotl González Morales <sixtogm@lcg.unam.mx>
2026-03-18 18:33:21 -0600

    feat: Now includes | TF | Total genes | Activados | Reprimidos | Genes | Tipo |

--- scr/regulon_summary.py
+++ scr/regulon_summary.py
@@ -10,12 +10,37 @@ regulon = {}
 
 for TF, gene, effect in interactions:
     if TF not in regulon:
-        regulon[TF] = []
-    regulon[TF].append(gene)
+        regulon[TF] = {
+            "total": 0,
+            "activados": 0,
+            "reprimidos": 0,
+            "genes": []  
+        }
+    
+    regulon[TF]["total"] += 1
+    regulon[TF]["genes"].append(gene)
+    
+    if effect == "+":
+        regulon[TF]["activados"] += 1
+    elif effect == "-":
+        regulon[TF]["reprimidos"] += 1
+
+print("| TF | Total genes | Activados | Reprimidos | Genes | Tipo |")
 
 for TF in sorted(regulon):
-    genes = sorted(regulon[TF])
-    total = len(genes)
-    lista_genes = ", ".join(genes)
-    print("|",TF,"|", total,"|", lista_genes,"|")
+    total = regulon[TF]["total"]
+    act = regulon[TF]["activados"]
+    rep = regulon[TF]["reprimidos"]
+    
+    genes_ordenados = sorted(regulon[TF]["genes"])
+    lista_genes = ", ".join(genes_ordenados)
+    
+    if act > 0 and rep > 0:
+        tipo = "dual"
+    elif act > 0:
+        tipo = "activador"
+    else:
+        tipo = "represor"
+    
+    print(f"| {TF} | {total} | {act} | {rep} | {lista_genes} | {tipo} |")
 
